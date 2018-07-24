da32a4c4c9a05774b0d6aba2f5ea3d30e537f102
sampaw13 <samarielpaw@gmail.com>
2018-07-24 14:45:31 +0000

    automatically backed up by learn

---
+++ control.rb
@@ -0,0 +1,11 @@
+def breakfast(food= "scrambled eggs")
+  "Morning is the right time for #{food}!"
+end
+
+def lunch(food= 'taco')
+  "Noon is the best time for #{food}!"
+end
+
+def dinner(food= "chicken")
+  "Nighttime is the best time for #{food}!"
+end
