c5b8a05b86f0a3a79e7f976892257e1fe5a75c82
sampaw13 <samarielpaw@gmail.com>
2018-07-24 14:47:07 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -12,24 +12,10 @@ def lunchoptions(breakfast, lunch, dinner)
 end
 
 
-def breakfast(food= "scrambled eggs")
-  "Morning is the right time for #{food}!"
+breakfast = "frosted flakes"
+lunch = "grilled cheese"
+dinner = "salmon"
+
+def lunchoptions(breakfast, lunch, dinner) 
+  puts "Morning is the best time for #{breakfast}! Noon is the best time for #{lunch}! Nighttime is the best time for #{dinner}!"
 end
-
-def lunch(food= 'taco')
-  "Noon is the best time for #{food}!"
-end
-
-def dinner(food= "chicken")
-  "Nighttime is the best time for #{food}!"
-end
-
-  
-
-
-
-
-
-# Call your methods without any arguments here
-puts snacks
-## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
