b4ab687d63ce4651aab71b3784e786d4d6c0d175
sampaw13 <samarielpaw@gmail.com>
2018-07-24 14:59:42 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -18,16 +18,4 @@ def lunchoptions(breakfast, lunch, dinner)
 end
 
 
-def breakfast(breakfast= "scrambled eggs")
-  "Morning is the right time for #{breakfast}!"
-end 
-
-def lunch(lunch= "taco")
-  "Noon is the best time for #{lunch}!"
-end
-
-def dinner(dinner= "chicken")
-  "Nighttime is the best time for #{dinner}!"
-end
-
 lunchoptions(breakfast, lunch, dinner)
