133f2206067a7c4ef8323774b7139bcb519201c5
sampaw13 <samarielpaw@gmail.com>
2018-07-24 14:19:26 +0000

    automatically backed up by learn

--- methodsp.rb
+++ methodsp.rb
@@ -4,3 +4,15 @@ birthplace = 'newyork'
 height = '63'
 
 puts my_name + 'was born in' + 'and is from' + 'and is' 
+
+puts 'What is your name?'
+my_name = gets 
+
+puts 'What is your birthdate?'
+birthdate = gets
+
+puts "Where were you born?"
+birthplace = gets
+
+puts 'How tall are you?'
+height = gets
